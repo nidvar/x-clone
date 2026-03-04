daec1460715758922ccf2496ead49db6a17a8772
nidvar <djayshine87@hotmail.com>
2026-03-04 12:51:18 +1100

    Clerk added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Hello Home Page with latest nextjs</h1>
+
+      <Show when="signed-out">
+
+        <SignInButton mode="modal">
+          <button className="bg-blue-500 m-2 p-2">
+            Sign In
+          </button>
+        </SignInButton>
+
+        <SignUpButton mode="modal">
+          <button className="bg-blue-500 m-2 p-2">
+            Sign Up
+          </button>
+        </SignUpButton>
+
+      </Show>
+      <Show when="signed-in">
+        <UserButton />
+      </Show>
+
     </div>
   );
 }
