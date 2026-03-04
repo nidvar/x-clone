fe2814f2ca0983f11c7239db997248593d167e37
nidvar <djayshine87@hotmail.com>
2026-03-04 12:35:36 +1100

    update to latest nextjs because of Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello Home Page</h1>
+      <h1>Hello Home Page with latest nextjs</h1>
     </div>
   );
 }
