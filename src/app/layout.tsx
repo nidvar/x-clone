bba9c6fc489ef6fd6640b2b1c9896cc6f0e8ce10
nidvar <djayshine87@hotmail.com>
2026-03-11 12:13:05 +1100

    sidebar added, card component installed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,7 +37,7 @@ export default function RootLayout({
             <main className="py-8">
               <div className="max-w-7xl mx-auto px-4">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-                  <div className="hidden lg:block lg:col-span-3">sidebar</div>
+                  <div className="hidden lg:block lg:col-span-3"><Sidebar /></div>
                   <div className="lg:col-span-9">{children}</div>
                 </div>
               </div>
